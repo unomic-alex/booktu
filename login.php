541ba73c66be296aa768e5a7368e5b8c9c9eaf09
shna231 <71744479+shna231@users.noreply.github.com>
2020-09-23 15:56:10 +0900

    Update login.php

--- login.php
+++ login.php
@@ -7,36 +7,40 @@ include('dbcon.php');
 
 
 //POST 값을 읽어온다.
-$id=isset($_POST['id']) ? $_POST['id'] : '';
+$id =isset($_POST['id']) ? $_POST['id'] : '';
 $password = isset($_POST['password']) ? $_POST['password'] : '';
 $android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
 
-// 계정정보 검색
-$sql="select id, password from member where id='$id' and password='$password'";
-$stmt = $con->prepare($sql);
-$stmt->execute();
 
-if ($stmt->rowCount() == 0) {
-	
-	echo "";
-	echo "아이디나 비밀번호를 확인하세요.";
+if ($id != "" ){ 
 
-}
-else {
+    $sql="select * from member where id='$id' and password='$password'";
+    $stmt = $con->prepare($sql);
+    $stmt->execute();
+ 
+    if ($stmt->rowCount() == 0){
 
-	
-	$data = array();
-	extract($row);
-	
-	echo $row["id"];
-	echo $row["password"];
-	
-	array_push($data,
-		array('id'=>$row["id"],
-		'password'=>$row["password"]	 
-	));
-	
-	
+        echo "'";
+        echo $id,", ",;
+        echo "'계정은 찾을 수 없습니다.";
+    }
+	else{
+
+   		$data = array(); 
+
+        while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+
+        	extract($row);
+
+            array_push($data, 
+                array('id'=>$row["id"],
+                'password'=>$row["password"],
+                'nickname'=>$row["nickname"],
+		'email'=>$row["email"]
+            ));
+        }
+
+
         if (!$android) {
             echo "<pre>"; 
             print_r($data); 
@@ -47,10 +51,15 @@ else {
             $json = json_encode(array("webnautes"=>$data), JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
             echo $json;
         }
-	
-	
+    }
 }
+else {
+    echo "계정 정보를 입력하세요. ";
+}
+
 ?>
+
+
 
 <?php
 
@@ -58,12 +67,13 @@ $android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
 
 if (!$android){
 ?>
+
 <html>
    <body>
    
       <form action="<?php $_PHP_SELF ?>" method="POST">
-         id: <input type = "text" name = "id" />
-         password: <input type = "text" name = "password" />
+         아이디: <input type = "text" name = "id" />
+         패스워드: <input type = "text" name = "password" />
          <input type = "submit" />
       </form>
    
