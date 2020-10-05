2088a628987fa45d181d16177e9e5c39ef2f8133
shna231 <71744479+shna231@users.noreply.github.com>
2020-10-06 01:09:50 +0900

    Update reviewinsert.php

--- reviewinsert.php
+++ reviewinsert.php
@@ -8,37 +8,33 @@
 
     $android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
 
-    print('print test');
-    echo('echo test');
-
 
     if( (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['submit'])) || $android )
     {
+
+        // 안드로이드 코드의 postParameters 변수에 적어준 이름을 가지고 값을 전달 받습니다.
 
         $writer_id=$_POST['writer_id'];
         $isbn=$_POST['isbn'];
         $title=$_POST['title'];
         $content=$_POST['content'];
-	    
-        if(!isset($errMSG))
+
+        if(!isset($errMSG)) 
         {
             try{
-                // SQL문을 실행하여 데이터를 MySQL 서버의 person 테이블에 저장합니다. 
                 $stmt = $con->prepare('INSERT INTO report(writer_id, isbn, title, content) VALUES(:writer_id, :isbn, :title, :content)');
                 $stmt->bindParam(':writer_id', $writer_id);
                 $stmt->bindParam(':isbn', $isbn);
                 $stmt->bindParam(':title', $title);
                 $stmt->bindParam(':content', $content);
-		    
+
                 if($stmt->execute())
                 {
                     $successMSG = "새로운 독후감을 추가했습니다.";
-			print $successMSG
                 }
                 else
                 {
                     $errMSG = "독후감 추가 에러";
-			print $errMSG
                 }
 
             } catch(PDOException $e) {
@@ -48,4 +44,31 @@
 
     }
 
+?>
+
+
+<?php 
+    if (isset($errMSG)) echo $errMSG;
+    if (isset($successMSG)) echo $successMSG;
+
+	$android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
+   
+    if( !$android )
+    {
+?>
+    <html>
+       <body>
+
+            <form action="<?php $_PHP_SELF ?>" method="POST">
+                Writer_id: <input type = "text" name = "writer_id" />
+                Isbn: <input type = "text" name = "isbn" />
+                Title: <input type = "text" name = "title" />
+                Content: <input type = "text" name = "content" />
+                <input type = "submit" name = "submit" />
+            </form>
+       
+       </body>
+    </html>
+<?php 
+    }
 ?>
