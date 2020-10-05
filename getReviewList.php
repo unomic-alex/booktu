3e46072e393db3bc06c96d2e13234ee78063caf0
shna231 <71744479+shna231@users.noreply.github.com>
2020-10-06 04:47:33 +0900

    Update getReviewList.php

--- getReviewList.php
+++ getReviewList.php
@@ -7,8 +7,8 @@ include('dbcon.php');
 //POST 값을 읽어온다.
 $writer_id=isset($_POST['writer_id']) ? $_POST['writer_id'] : '';
 
-//사용자의 독후감 리스트 읽어오기.
-$sql="select * from report where writer_id='$writer_id'";
+//사용자의 독후감 리스트 읽어오기. where writer_id='$writer_id'
+$sql="select * from report";
 
 $stmt=$con->prepare($sql);
 $stmt->execute();
