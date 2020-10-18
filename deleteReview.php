c991056e663dd62141327a28ac1558d2d65a6bac
shna231 <71744479+shna231@users.noreply.github.com>
2020-10-18 18:04:30 +0900

    Create deleteReview.php

---
+++ deleteReview.php
@@ -0,0 +1,27 @@
+<?php  
+error_reporting(E_ALL); 
+ini_set('display_errors',1); 
+
+include('dbcon.php');
+
+//POST 값을 읽어온다.
+$title=isset($_POST['title']) ? $_POST['title'] : '';
+
+$sql="delete from report where title='$title'";
+
+$stmt=$con->prepare($sql);
+$stmt->execute();
+ 
+if ($stmt->rowCount() == 0){
+    echo "계정을 찾을 수 없습니다.";
+	
+} else{
+
+   	echo "success";
+	}
+	
+	header('Content-Type: application/json; charset=utf8');
+	echo "string return";
+    } 
+
+?>
