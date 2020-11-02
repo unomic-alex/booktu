7c6b521811ceebfbb5b6b65dcb8d1d8f050d5bde
shna231 <71744479+shna231@users.noreply.github.com>
2020-11-02 17:28:43 +0900

    Update deleteReview.php

--- deleteReview.php
+++ deleteReview.php
@@ -7,7 +7,7 @@ include('dbcon.php');
 //POST 값을 읽어온다.
 $article_num=isset($_POST['article_num']) ? $_POST['article_num'] : '';
 
-$sql="DELETE FROM booktu.report WHERE article_num='$article_num";
+$sql="DELETE FROM booktu.report WHERE article_num='$article_num'";
 
 $stmt=$con->prepare($sql);
 $stmt->execute();
